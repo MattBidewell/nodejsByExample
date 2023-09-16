dde11f2c3ba0220e56640f59407b13cf0c12948a
MattBidewell <matthewbidewell@gmail.com>
2023-09-16 10:09:01 +0100

    automatically generate pages based on code examples

--- tools/generate.js
+++ tools/generate.js
@@ -3,18 +3,9 @@ import path from "path";
 import pug from "pug";
 import hljs from 'highlight.js';
 
-// import 'highlight.js/styles/github.css';
-
 const siteDir = "./public";
 const templateDir = "./templates";
-
-// read file line by line from example directory
-
-const templateFiles = fs.readdirSync(templateDir);
-
-renderSinglePage("index");
-
-renderCodePages();
+const examplesDir = "./examples";
 
 function renderSinglePage(filePath, contents, filenameOverride) {
   if (!filePath.endsWith(".pug")) {
@@ -24,7 +15,7 @@ function renderSinglePage(filePath, contents, filenameOverride) {
   const template = fs.readFileSync(templatePath, "utf8");
   const html = pug.render(template, { pretty: true, filename: templatePath, contents });
 
-  if (!filenameOverride) {
+  if (filenameOverride) {
     filePath = `${filenameOverride}.pug`;
   }
   const htmlFile = filePath.replace(".pug", ".html");
@@ -33,11 +24,66 @@ function renderSinglePage(filePath, contents, filenameOverride) {
 }
 
 function renderCodePages() {
-  const contents = [
-    {
-      comment: "This is hello world!\n\nThis is a second paragraph.",
-      code: hljs.highlight("function helloWorld() {\n  return 'Hello World!';\n}", { language: "javascript" }).value,
+
+  // read contents of examples directory nested directories
+  // const contents = [];
+  const dirs = fs.readdirSync(examplesDir, { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+
+  for (const dir of dirs) {
+    const dirPath = path.join(examplesDir, dir.name);
+    const files = fs.readdirSync(dirPath, { withFileTypes: true }).filter(file => file.isFile())
+    const codeFile = files.find(file => file.name.endsWith(".js"));
+    const scriptFile = files.find(file => file.name.endsWith(".sh"));
+
+    const content = {
+      name: dir.name,
+      code: fs.readFileSync(path.join(dirPath, codeFile.name), "utf8"),
+      script: fs.readFileSync(path.join(dirPath, scriptFile.name), "utf8"),
     }
-  ]
-  renderSinglePage("content", contents, "helloworld");
+
+    const contents = [];
+    // read the code file and split it into sections
+    const codeSections = content.code.split("\n\n\n");
+
+    for (const section of codeSections) {
+      let comment = "";
+      let code = "";
+      const lines = section.split("\n");
+      for (const line of lines) {
+        if (line.startsWith("//")) {
+          comment += line.replace("//", "") + "\n";
+        } else {
+          code += line + "\n";
+        }
+      }
+      contents.push({
+        comment,
+        code: hljs.highlight(code, { language: "javascript" }).value,
+      });
+
+    }
+    // read the script file and split it into sections
+    const scriptSections = content.script.split("\n\n\n");
+    for (const section of scriptSections) {
+      let comment = "";
+      let script = "";
+      const lines = section.split("\n");
+      for (const line of lines) {
+        if (line.startsWith("//")) {
+          comment += line.replace("//", "") + "\n";
+        } else {
+          script += line + "\n";
+        }
+      }
+      contents.push({ // I think this is the wrong way round
+        comment,
+        code: hljs.highlight(script, { language: "shell", ignoreIllegals: true }).value,
+      });
+    }
+    renderSinglePage("content", contents, dir.name);
+  }
 }
+
+renderSinglePage("index");
+
+renderCodePages();
